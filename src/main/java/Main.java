e9016b5a893448ce3b72dac009cd0603e01ce437
AndrewNR <andrey.stop4enko@gmail.com>
2015-06-25 16:51:55 +0300

    Configured the OAuth-related piece of web app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
 //import org.eclipse.jetty.server.Server;
 //import org.eclipse.jetty.servlet.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
+@SuppressWarnings("serial")
 public class Main extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
